f0a8ffa555d675facb0cb236d3aa30ee9e73ed4d
tian sheng <siewtiansheng@gmail.com>
2023-01-28 14:28:50 +0800

    add global css variables

---
+++ src/styles/variables.ts
@@ -0,0 +1,16 @@
+export const globalCSSVariables = {
+    primaryColor: "#1e3a8a",
+    secondaryColor: "grey",
+    successColor: "#059669",
+    warningColor: "#FCD34D",
+    errorColor: "#DC2626",
+    bgColor: "#f8fafc",
+    black: "#000000",
+    lightGrey: "#eee",
+    white: "#ffffff",
+    highlight: "yellow",
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+    primaryFf: '"Monserrat", sans-serif',
+    secondaryFf: '"Poppins", sans-serif',
+    codeFf: '"Courier New", Courier, monospace',
+};
